882d56cf2cc6a23761b5436f135f8d736eb264a4
niedu02 <duane.nielsen.rocks@gmail.com>
2018-10-22 18:29:58 -0700

    added requirements.txt

--- error_histogram.py
+++ error_histogram.py
@@ -27,13 +27,30 @@ full_loader = DataLoader(test, batch_size=batch_size, drop_last=False)
 
 
 error = []
+ground_truth = []
 batch = tqdm(full_loader, total=len(dataset) // batch_size)
 for signal, target_label in batch:
     signal, target_label = signal.to(device), target_label.to(device)
     estimate = model(signal.unsqueeze(1))
+    ground_truth.append(target_label.detach())
     err = estimate - target_label
-    error.append(err.data.cpu().numpy())
+    error.append(err.detach())
 
-plt.hist(error)
+
+error = torch.cat(error)
+ground_truth = torch.cat(ground_truth)
+
+plt.hist(error.cpu().numpy(), bins=30)
+plt.xlabel('error')
+plt.ylabel('samples')
+plt.show()
+
+
+x = ground_truth.cpu().numpy()
+y = error.data.cpu().numpy()
+
+plt.scatter(x, y)
+plt.xlabel('thickness')
+plt.ylabel('error')
 plt.show()
 
